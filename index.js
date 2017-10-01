c2dc8bb1b9e80737ec62b37d804b485b958bc134
Nolan Meeks <nolanm4509@gmail.com>
2017-10-01 15:36:51 -0400

    comit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let fetch = require('node-fetch');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
