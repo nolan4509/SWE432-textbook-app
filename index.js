1450f34e929d803260abbd7e71de7a2593154751
John <hunt2882@yahoo.com>
2017-10-03 16:40:59 -0400

    hw4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 Scenarios:
 	(Buyers)
 		-Search for a Course
-			GET /courses/:courseID
+			GET /courses/:courseCode/:courseLevel
 		-Retrieve textbook post information
 			GET /courses/:courseID/:postID
 		-Request to purchase textbook
@@ -27,8 +27,8 @@ Scenarios:
 
 class Course {
     constructor(code, level, id) {
-        this.code = code; //101 , 102, etc.
-        this.level = level; //CS, MATH, etc.
+        this.code = code.toLowerCase(); //CS, MATH, etc.
+        this.level = level; //101 , 102, etc.
         this.id = id;
     }
     name() {
@@ -56,18 +56,63 @@ class User {
 }
 
 class BookPost {
-	constructor(textbook, id, condition, seller, teacherName, price) {
+	constructor(textbook, id, condition, seller, teacherName, price, course) {
 		this.textbook = textbook;
 		this.id = id;
 		this.condition = condition;
 		this.seller = seller;
 		this.teacherName = teacherName;
 		this.price = price;
+		this.course = course;
 	}
 }
 
-app.get('/', function(request, response) {
-    response.send("Hello World")
+const firebase = require("firebase");
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBYVmMA2jzaP7B6R3V-6xbxtJ8109C_UPk",
+    authDomain: "swe432-29722.firebaseapp.com",
+    databaseURL: "https://swe432-29722.firebaseio.com",
+    projectId: "swe432-29722",
+    storageBucket: "",
+    messagingSenderId: "957222857407"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
+database.ref('users/' + 'Josh').set(
+    { username: 'Josh', email: 'josh@gmail.com'
+    });
+
+//test data
+let testCsCourse = new Course('CS', 101, 0);
+let testBook = new Textbook('test book', 9871234567890, 'titletest', 'yolo', 8);
+let testUser = new User('John', 'jhunt11', 'jhunt11@gmu.edu', []);
+let bookPostArray = [];
+bookPostArray[0] = new BookPost(testBook, 123, 'good', testUser, 'Prof. Test', 999999, testCsCourse);
+//end test data
+app.get('/courses/:courseCode/:courseLevel', function(req, res){
+    let courseLevel = Number(req.params.courseLevel);
+    let courseCode = String(req.params.courseCode).toLowerCase();
+    let courseID = courseCode + courseLevel;
+    let retCourses = [];
+    let count = 0;
+
+    bookPostArray.map(function(course) { //search through bookPostArray for matching course
+        if(course.course.name() === courseID){
+            retCourses[count] = course;
+        }
+    });
+    if(retCourses.length === 0){
+        res.send("No books found");
+        return;
+    }
+    res.send(retCourses);
+});
+
+app.get('/', function(req, res) {
+    res.send(testCsCourse.name())
+    //res.send("Hello World");
 });
 
 
