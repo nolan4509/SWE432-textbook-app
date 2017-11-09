c81303d830540e9c1a7e22c9ced8e687e232322b
Nolan Meeks <nolanm4509@gmail.com>
2017-11-09 10:10:11 -0500

    same

--- index.js
+++ index.js
@@ -352,9 +352,9 @@ app.use(express.static(path.join(__dirname, 'react/client/public')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-//app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname + '/react/client/public/index.html'));
-//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/react/client/public/index.html'));
+});
 
 
 
