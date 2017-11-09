2fd5fe0000fb87c398805e46c16d9d81ad7151b3
Nolan Meeks <nolanm4509@gmail.com>
2017-11-09 08:21:14 -0500

    Login

--- react/client/src/containers/login.js
+++ react/client/src/containers/login.js
@@ -5,10 +5,45 @@ import '../style.css';
 class Login extends Component {
     constructor(props) {
         super(props)
-
+        this.handleChangeEmail = this.handleChangeEmail.bind(this)
+        this.handleChangeName = this.handleChangeName.bind(this)
+        this.state = {
+            userID: '',
+            userName: '',
+            email: ''
+        }
     }
 
+    homeClick = () => {
+        this.props.history.push("/")
+    }
 
+    login = () => {
+
+        fetch(`/add/user/${this.state.userName}/${this.state.userID}/${this.state.email}`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        }).then(alert('Post request sent to server.'))
+            .catch((ex) => {
+                console.log('parsing failed', ex)
+            })
+    }
+
+    handleChangeEmail(event) {
+        this.setState({
+            email : event.target.value,
+            userID : event.target.value.substr(0, event.target.value.indexOf('@'))
+        })
+    }
+
+    handleChangeName(event) {
+        this.setState({
+            userName : event.target.value
+        })
+    }
 
     render() {
         return (
@@ -20,10 +55,16 @@ class Login extends Component {
                     <h1>Sign In</h1>
                 </header>
                 <div id="loginForm">
-                    <form action="/home" method="get">
+                    <form onSubmit={this.login}>
+                        <div>
+                            <label htmlFor="userName">First Name: </label>
+                            <input id="userName" type="text" placeholder="Ex. 'John'" required
+                                value={this.state.userName} onChange={this.handleChangeName}/>
+                        </div>
                         <div>
                             <label htmlFor="email">Email: </label>
-                            <input id="email" type="email" placeholder="Enter your Email"/>
+                            <input id="email" type="email" placeholder="Enter your Email" required
+                                value={this.state.email} onChange={this.handleChangeEmail}/>
                         </div>
                         <br/>
                         <div>
